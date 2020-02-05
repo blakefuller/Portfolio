32edd4b4937c06e2a944038abc5abd84bbfbb56a
blakefuller <bfuller3134@gmail.com>
2020-02-04 22:43:56 -0800

    fixed icons and cursor

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,20 @@
-// const skillsLink = document.querySelector(".page-header li:nth-child(1) a");
-const skillsLink = document.getElementById("skills");
-const skillsClose = document.querySelector(".skills-close");
-const windowOpened = "window-opened";
-const closePage = document.getElementById("close");
+// const skillsLink = document.getElementById("skills");
+// const skillsClose = document.querySelector(".skills-close");
+// const windowOpened = "window-opened";
+// const closePage = document.getElementById("close");
 
-skillsLink.addEventListener("click", (e) => {
-  e.preventDefault();
-  document.body.classList.toggle(windowOpened);
-});
+// skillsLink.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   document.body.classList.toggle(windowOpened);
+// });
 
-skillsClose.addEventListener("click", () => {
-  document.body.classList.toggle(windowOpened);
-});
+// skillsClose.addEventListener("click", () => {
+//   document.body.classList.toggle(windowOpened);
+// });
 
-closePage.addEventListener("click", () => {
-  document.close();
-});
+// closePage.addEventListener("click", () => {
+//   document.close();
+// });
 
 //cursor animation
 var cursor = document.getElementById('cursor');
